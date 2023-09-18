36cd29d80e211b9d1e0f3fdc73710559cb937649
Yilun Liu <hi@allenliu.xyz>
2023-09-18 13:14:24 +1000

    Add activation.

--- scripts/table2.sh
+++ scripts/table2.sh
@@ -18,7 +18,7 @@ python train.py \
 --result-path /scratch/itee/uqyliu71/CaT/results \
 --dataset-name $dataset \
 --budget $budget \
---cgm-args "{'n_encoders': 5000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1}";
+--cgm-args "{'n_encoders': 5000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1, 'activation': True}";
 done
 
 # Arxiv
@@ -37,7 +37,7 @@ python train.py \
 --result-path /scratch/itee/uqyliu71/CaT/results \
 --dataset-name $dataset \
 --budget $budget \
---cgm-args "{'n_encoders': 3000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1}";
+--cgm-args "{'n_encoders': 3000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1, 'activation': True}";
 done
 
 
@@ -57,7 +57,7 @@ python train.py \
 --result-path /scratch/itee/uqyliu71/CaT/results \
 --dataset-name $dataset \
 --budget $budget \
---cgm-args "{'n_encoders': 3000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1}";
+--cgm-args "{'n_encoders': 3000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1, 'activation': True}";
 done
 
 # Reddit
@@ -76,5 +76,26 @@ python train.py \
 --result-path /scratch/itee/uqyliu71/CaT/results \
 --dataset-name $dataset \
 --budget $budget \
---cgm-args "{'n_encoders': 3000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1}";
+--cgm-args "{'n_encoders': 3000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1, 'activation': True}";
+done
+
+seed=1024
+repeat=1
+epoch=500
+dataset=arxiv
+budget=29
+
+for method in cgm
+do
+python train.py \
+--seed $seed \
+--repeat $repeat \
+--cls-epoch $epoch \
+--cgl-method $method \
+--tim \
+--data-dir /scratch/itee/uqyliu71/CaT/data \
+--result-path /scratch/itee/uqyliu71/CaT/results \
+--dataset-name $dataset \
+--budget $budget \
+--cgm-args "{'n_encoders': 1000, 'feat_init': 'randomChoice', 'feat_lr': 1e-3, 'hid_dim': 512, 'emb_dim': 256, 'n_layers': 2, 'hop': 1, 'activation': True}";
 done
